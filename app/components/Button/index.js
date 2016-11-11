9c3b2f716b3fb57cfa65c95cd9e53c9f7f7fccee
Kevin Wu <kevinwu127@gmail.com>
2016-11-11 15:48:06 -0800

    added necessary components for ARK home page

---
+++ app/components/Button/index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * Button.react.js
+ *
+ * A common button, if you pass it a prop "route" it'll render a link to a react-router route
+ * otherwise it'll render a link with an onclick
+ */
+
+import React, { PropTypes, Children } from 'react';
+
+import A from './A';
